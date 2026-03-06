7110902301100114660e598d080065b299549d7a
mtr <mtr@DESKTOP-3J5TKNU.bbrouter>
2026-03-06 18:01:20 +0530

    right test

--- Java2026Practice/src/JavaBasics/HelloWorld.java
+++ Java2026Practice/src/JavaBasics/HelloWorld.java
@@ -4,14 +4,14 @@ public class HelloWorld {
 	
 	public static void main(String[] args) {
 		
-System.out.println("Hello World");
-System.out.println("Hello World");
+System.out.println("Hello gg World");
+System.out.println("Hello g World");
 
 
 //System.out.println("Hello World");  Single line comment
 
 
-// Multi line comment.
+// Multi line comment.right?
 /*System.out.println("Hello World");    
   System.out.println("Hello World");
  System.out.println("Hello World"); */  
