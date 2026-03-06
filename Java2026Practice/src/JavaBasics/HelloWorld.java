c5e122d42fad8a3243da9e5f346cb1482cb09317
mtr <mtr@DESKTOP-3J5TKNU.bbrouter>
2026-03-06 17:27:47 +0530

    ye

---
+++ Java2026Practice/src/JavaBasics/HelloWorld.java
@@ -0,0 +1,24 @@
+package JavaBasics;
+
+public class HelloWorld {
+	
+	public static void main(String[] args) {
+		
+System.out.println("Hello World");
+System.out.println("Hello World");
+
+
+//System.out.println("Hello World");  Single line comment
+
+
+// Multi line comment
+/*System.out.println("Hello World");    
+  System.out.println("Hello World");
+ System.out.println("Hello World"); */  
+
+
+		
+		
+	}
+
+}
